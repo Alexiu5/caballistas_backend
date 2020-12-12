ee0d298fa1d23eca9446630a87bcc2dc3cb8bc6b
Alex <alexius9000@gmail.com>
2020-12-12 15:27:33 -0500

    default changes

--- configs/index.js
+++ configs/index.js
@@ -1,5 +1,5 @@
 "use strict";;
-const env = process.env.NODE_ENV || 'production';
+const env = process.env.NODE_ENV || 'local';
 const defaultConfig = require(`./${env}`);
 
 module.exports = defaultConfig;
